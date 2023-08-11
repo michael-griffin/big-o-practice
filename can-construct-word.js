981d298c9203f8c74914fb00ecef2ef24d5364d7
Evan Ishibashi <evan.ishibashi@gmail.com>
2023-08-11 15:21:01 -0700

    completed can-construct-word

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,44 @@
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+
+// Can you spell the word using the available letters?
+
+//Inputs word + letters
+
+//Output boolean: (t or f)
+
+// letters
+
+
+// evaluate the available letters, record into obj
+// Check if letters in word are available in obj
+// Return true if you are able to spell word,
+    //else return false
+
+
+
+
+
+function canConstructWord(word, letters) {
+  // evaluate the available letters, record into obj
+  const wordBank = {};
+  const letterBank = {};
+
+  for (let char of word){
+    const charCount = (wordBank[char] || 0) + 1;
+    wordBank[char] = charCount;
+  }
+
+  for (let char of letters){
+    const charCount = (letterBank[char] || 0) + 1;
+    letterBank[char] = charCount;
+  }
+
+  // Check if letters in word are available in obj
+  for (let key in wordBank){
+    if(wordBank[key] > letterBank[key]
+       || (!letterBank[key])){
+      return false;
+    }
+  }
+  return true;
 }
