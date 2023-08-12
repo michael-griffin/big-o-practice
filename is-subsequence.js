862336482850eef15dde04576808c0444059c011
Evan Ishibashi <evan.ishibashi@gmail.com>
2023-08-11 17:18:10 -0700

    completed separate-positive,subsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,55 @@
+"use strict";
+
 // add whatever parameters you deem necessary & write docstring
-function isSubsequence() {
+
+//This should take two strings, s1 and s2.
+//It checks whether the characters in s1`form
+//a subsequence of the characters in `s2.
+//In other words, this should check whether the characters
+//in the s1 appear somewhere in the s2, without their order changing.
+
+//Constraints: Time complexity: O(n + m)
+
+
+//Check if the characters in string1, in order, appear in that same order,
+//in string 2, even if there are other characters inbetween.
+
+//inputs: string1, string2
+//output: boolean: t or f
+
+
+
+
+/**
+ * Takes in two strings, and checks if the characters in String1
+ * appear in the same order in String2, regardless if there are other
+ * chars in between
+ *
+ * @param {*} word1
+ * @param {*} word2
+ * @returns true if true, false if not.
+ */
+function isSubsequence(word1, word2) {
+  //see if chars in string1 appear in order in string2
+  let wordCounter1 = 0; //update name to index1
+  let wordCounter2 = 0;
+
+
+  while (wordCounter1 < word1.length && wordCounter2 < word2.length) {
+    if (word1[wordCounter1] === word2[wordCounter2]) {
+      wordCounter1++;
+      wordCounter2++;
+    }
+    else {
+      wordCounter2++;
+    }
+  }
+
+
+  //return t or f
+  return (wordCounter1 === word1.length) ? true: false; //doesnt need the ternary piece
 }
+
+isSubsequence('hello', 'hello world'); // true
+isSubsequence('sing', 'sting');        // true
+isSubsequence('abc', 'acb');           // false (order matters)
--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,58 @@
+"use strict"
 // add whatever parameters you deem necessary & write docstring
-function separatePositive() {
+
+// This should take an array of non-zero numbers.
+// Separate the positive numbers to the left and the negative numbers to the right.
+// The positive numbers and negative numbers need not be in sorted order.
+
+// The problem should be done in place,
+// and the function should return the original array, not a copy.
+
+//Constraints: Time complexity: O(n)
+
+
+// Without going outside of original array/making a copy, move all positives
+// to the left, all negative nums to the right.
+
+//Inputs: Array of non-zero numbers
+//Output: SAME array, but sorted.
+
+//Iterate through array only once, moving negatives to the right.
+
+// return sorted array
+
+
+/**
+ * Takes in an array of non-zero numbers and
+ * sorts the positive numbers to the left
+ * and negative numbers on the right.
+ *
+ * @param {*} nums
+ * @returns sorted nums array
+ */
+function separatePositive(nums) {
+  //Iterate through array only once, moving negatives to the right.
+  let counter = 0;
+  let index = 0;
+
+
+  while(counter < nums.length){
+    if(nums[index] < 0){
+      const negativeNum = nums.splice(index,1); //splice causes this to be quadratic
+      //[nums[start], nums[end]] = [nums[end], nums[start]];
+      nums.push(negativeNum[0]);
+      counter++;
+    }
+    else {
+      counter++;
+      index++
+    }
+  }
+
+  // return sorted array
+  return nums;
 }
+
+separatePositive([2, -1, -3, 6]);         // [2, 6, -3, -1]
+separatePositive([-5, 5]);                // [5, -5]
+separatePositive([1, 2, 3]);              // [1, 2, 3]
